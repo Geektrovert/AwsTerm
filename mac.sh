f4b68a2206348f1d6144e892ad3a51ddbc5d7eea
Samnan Rahee <namanush.rsr.16@gmail.com>
2019-03-10 01:12:03 +0600

    support for macOS and linux added

---
+++ mac.sh
@@ -0,0 +1,21 @@
+brew install htop
+brew install bat
+brew install exa
+brew install fd
+brew install ripgrep
+brew install tig
+brew install thefuck
+echo 'eval $(thefuck --alias)' >> .*rc
+brew install httpie
+brew install asciinema
+brew install neofetch
+brew install pstree
+# installing hexyl
+curl -L -o hexyl.tar.gz https://github.com/sharkdp/hexyl/releases/download/v0.3.0/hexyl-v0.3.0-x86_64-apple-darwin.tar.gz
+tar -xf hexyl.tar.gz
+mv hexyl-v0.3.0-x86_64-apple-darwin/hexyl /usr/local/bin
+chmod +x /usr/local/bin/hexyl
+rm hexyl.tar.gz
+rm -rf hexyl-v0.3.0-x86_64-apple-darwin/
+
+source ~/.*rc
---
+++ ubuntu.sh
@@ -0,0 +1,17 @@
+apt-get update htop
+apt-get update bat
+apt-get update exa
+apt-get update fd
+apt-get update ripgrep
+apt-get update tig
+apt-get update thefuck
+echo 'eval $(thefuck --alias)' >> .*rc
+apt-get update httpie
+apt-get update asciinema
+apt-get update neofetch
+apt-get update pstree
+# installing hexyl
+wget "https://github.com/sharkdp/hexyl/releases/download/v0.4.0/hexyl_0.4.0_amd64.deb"
+dpkg -i hexyl_0.4.0_amd64.deb
+
+source ~/.*rc
