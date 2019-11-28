cabc37e97fcb61a79be9d813a6f019196a458991
Minix <minix@localhost.localdomain>
2019-11-29 02:32:09 +0600

    fixed

--- fedora.sh
+++ fedora.sh
@@ -9,8 +9,7 @@ sudo dnf install htop bat exa ripgrep tig httpie asciinema neofetch nnn aria2 mc
 # the fuck
 sudo dnf install python-pip python-setuptools
 sudo pip install thefuck
-sudo echo 'eval $(thefuck --alias)' >>~/.bashrc # redirect to home
-sudo echo 'eval $(thefuck --alias)' >>~/.zshrc  # redirect to home
+
 
 # hexyl-A cmd hex viewer(requires Rust version >= 1.31)
 sudo dnf install cargo
@@ -18,7 +17,7 @@ cargo install hexyl
 sudo dnf remove cargo
 
 # rebound(Command-line tool that instantly fetches Stack Overflow results when an exception is thrown)
-pipenv install rebound-cli
+sudo pip install rebound-cli
 
 # update source
 source ~/.*rc
