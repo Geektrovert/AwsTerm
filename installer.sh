ade6fd1719fa0b860d0d1ddc5f112e62553dea3e
Restyled.io <commits@restyled.io>
2021-08-14 18:28:03 +0000

    Restyled by shfmt

--- installer.sh
+++ installer.sh
@@ -6,21 +6,21 @@ fedora="fedora"
 arch="arch"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	if [[ -f /etc/os-release ]]; then
-		. /etc/os-release
-		OS=$ID_LIKE
-		if [[ "$OS" == *"$debian"* || "$OS" == *"$ubuntu"* ]]; then
-			sudo chmod +x deb.sh
-            ./deb.sh
-        elif [[ "$OS" == *"$arch"* ]]; then
-            sudo chmod +x arch.sh
-            ./arch.sh
-        elif [[ "$OS" == *"$fedora"* ]]; then
-            sudo chmod +x fedora.sh
-            ./fedora.sh
-        fi
-	fi
+  if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS=$ID_LIKE
+    if [[ "$OS" == *"$debian"* || "$OS" == *"$ubuntu"* ]]; then
+      sudo chmod +x deb.sh
+      ./deb.sh
+    elif [[ "$OS" == *"$arch"* ]]; then
+      sudo chmod +x arch.sh
+      ./arch.sh
+    elif [[ "$OS" == *"$fedora"* ]]; then
+      sudo chmod +x fedora.sh
+      ./fedora.sh
+    fi
+  fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    sudo chmod +x mac.sh
-    ./mac.sh
+  sudo chmod +x mac.sh
+  ./mac.sh
 fi
