7d1b5a1221281e0050745746c9b895bac5b72175
Farhan Sadik <45653000+farhansadik@users.noreply.github.com>
2021-09-01 00:41:56 +0600

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 #!/data/data/com.termux/files/usr/bin/bash
 
+# test 0.2
+# termux 0.117 required
+
 # basic 
 pkg update && pkg upgrade 
 pkg install python3
 
 # repo 
-pkg install htop bat exa fd ripgrep	tig asciinema neofetch hexyl nnn aria2 mc mtr pydf lftp fzf	psmisc
+pkg install htop bat exa fd ripgrep	tig asciinema neofetch hexyl nnn aria2 mc lftp fzf psmisc
+# missing mtr(https://github.com/traviscross/mtr)
 
 # pip packages 
 pip install
@@ -15,14 +19,13 @@ pkg upgrade
 
 # the fuck 
 pip3 install thefuck
+pip3 install pydf
 
 echo 'eval $(thefuck --alias)' >> $HOME/.bashrc
 echo 'eval $(thefuck --alias)' >> $HOME/.zshrc
 
 # httpie 
 pip install httpie 
-
-#check 
 
 termux-reload-settings
 exit 0
