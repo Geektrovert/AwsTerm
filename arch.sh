426253d3e1bc099ab06db2f682d483be2e004c3d
Farhan Sadik <farhansadikriduan@gmail.com>
2021-09-02 18:42:32 +0600

    added new packages

--- arch.sh
+++ arch.sh
@@ -5,16 +5,11 @@ echo "Synchronizing package databases"
 sudo pacman -Sy
 
 # packages
-sudo pacman -S htop bat exa fd ripgrep tig httpie asciinema neofetch hexyl nnn aria2 mc mtr pydf lftp
+sudo pacman -S htop bat exa fd ripgrep tig httpie asciinema neofetch hexyl nnn aria2 mc mtr pydf lftp fzf psmisc
 
 # packages new
 sudo pacman -S sl cowsay nethogs googler lolcat boxes figlet toilet cmatrix ranger
 
-# pstree
-sudo pacman -S psmisc
-
-# fzf(fzf is a general-purpose command-line fuzzy finder)
-sudo pacman -S fzf
 
 # thefuck
 sudo pacman -S python-pip
@@ -25,4 +20,4 @@ sudo echo 'eval $(thefuck --alias)' >> ~/.zshrc  # redirect to home
 # update source
 source ~/.*rc
 
-exit 0;
+exit 0
