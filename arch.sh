bb7f35706e33b24e9c871b592e1866de19f6ea46
Restyled.io <commits@restyled.io>
2021-09-02 12:48:12 +0000

    Restyled by shfmt

--- arch.sh
+++ arch.sh
@@ -10,12 +10,11 @@ sudo pacman -S htop bat exa fd ripgrep tig httpie asciinema neofetch hexyl nnn a
 # packages new
 sudo pacman -S sl cowsay nethogs googler lolcat boxes figlet toilet cmatrix ranger
 
-
 # thefuck
 sudo pacman -S python-pip
 sudo pip3 install thefuck
-sudo echo 'eval $(thefuck --alias)' >> ~/.bashrc # redirect to home
-sudo echo 'eval $(thefuck --alias)' >> ~/.zshrc  # redirect to home
+sudo echo 'eval $(thefuck --alias)' >>~/.bashrc # redirect to home
+sudo echo 'eval $(thefuck --alias)' >>~/.zshrc  # redirect to home
 
 # update source
 source ~/.*rc
